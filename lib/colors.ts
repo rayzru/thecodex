4d1441e6179dc67111cd838d13bc9163218850b1
Andrew Rumm <rayz@rayz.ru>
2022-05-13 01:45:31 +0300

    More unused

--- lib/colors.ts
+++
@@ -1,9 +0,0 @@
-import { pSBC } from './pSBC';
-
-export const darken = (color: string) => pSBC(-0.3, color);
-
-export const colors = ['#8091a5'];
-// , '#DB6B88', '#B9789F'
-
-export const randomColor = () =>
-  colors[Math.floor(Math.random() * colors.length)];
